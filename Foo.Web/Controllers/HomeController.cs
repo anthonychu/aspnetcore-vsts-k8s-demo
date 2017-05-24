b1705fdcfd8ec2c196d2db6e39b178b6943b6e25
Anthony Chu <anthony@anthonychu.ca>
2017-05-23 22:05:58 -0700

    Small change

--- Foo.Web/Controllers/HomeController.cs
+++ Foo.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Foo.Web.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Hello there!";
 
             return View();
         }
